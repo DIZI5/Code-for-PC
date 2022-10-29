c2923d95c91f996500a74810eac6dde33df4e262
HAKER0055 <qweadszxc88@gmail.com>
2022-10-29 23:34:03 +0300

    Fake virus

---
+++ virus.bat
@@ -0,0 +1,6 @@
+@echo off
+color 0a
+title ---------------HAKER---------------
+:x
+echo %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random% 
+goto x
